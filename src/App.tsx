a75150cafa5f68f45eb921b9bd9c825605b60183
Emeka Orji <emekapraiseo@gmail.com>
2024-10-13 19:55:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -59,9 +59,9 @@ function App() {
 		startOnLoad: false,
 	});
 
-	// useEffect(() => {
-	// 	start();
-	// }, [start]);
+	useEffect(() => {
+		start();
+	}, [start]);
 
 	// console.log(isEqualArrays([], []));
 
